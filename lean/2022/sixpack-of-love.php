fce6601e88793abe613a843e43a02e36052f9706
Michael Zumstein <zumi@yokai.ch>
2023-12-21 19:10:24 +0100

    feat: subscription is now called sixpack-of-love

---
+++ lean/2022/sixpack-of-love.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+        <link href="https://fonts.googleapis.com/css?family=Droid+Sans&display=swap"/>
+        <title>Satellites of Love Sixpack 👾 2022 — Limited outer space edition of 250 sixpacks</title>
+        <meta name="view-transition" content="same-origin">
+        <meta name="description" content="Sixpack of Love 👾 2022 — Limited outer space edition of 250 sixpacks">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width">
+        <meta name="geo.region" content="CH-BE" />
+        <meta name="geo.placename" content="Bern" />
+        <meta name="geo.position" content="46.950256;7.443965" />
+        <meta name="ICBM" content="46.950256, 7.443965" />
+        <meta name="geo.region" content="CH-VS" />
+        <meta name="geo.placename" content="Brentjong, Leuk/Loèche, Wallis/Valais" />
+        <meta name="geo.position" content="46.317660;7.652257" />
+        <meta name="ICBM" content="46.317660, 7.652257" />
+        <meta property="og:site_name" content="brentjong.space">
+        <meta property="og:title" content="Sixpack of Love 👾 2022 — Limited outer space edition of 250 sixpacks" />
+        <meta property="og:description" content="Order your limited outer space edition sixpack and support Satellites of Love." />
+        <meta property="og:image" itemprop="image" content="https://brentjong.space/apple-touch-icon.png">
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="de_CH" />
+        <meta name="theme-color" content="#ec0dad42"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <style type="text/css">
+        
+        @keyframes grow-xy {
+                from {
+                        transform: scaleX(0) scaleY(0);
+                }
+                to {
+                        transform: scaleX(1) scaleY(1);
+                }
+        }
+
+        @keyframes shrink-xy {
+                from {
+                        transform: scaleX(1) scaleY(1);
+                }
+                to {
+                        transform: scaleX(0) scaleY(0);
+                }
+        }
+
+        ::view-transition-old(*),
+        ::view-transition-new(*) {
+                height: auto;
+                right: 0;
+                left: auto;
+        }
+
+        ::view-transition-old(*) {
+                animation: 0.55s ease-out both shrink-xy;
+                transform-origin: right top;
+        }
+
+        ::view-transition-new(*) {
+                animation: 0.55s 0.55s ease-in both grow-xy;
+                transform-origin: right bottom;
+        }
+       
+       * {
+                margin: 0;
+                padding: 0;
+        }
+        body {
+                --brentjong-space--color--base: #fff;
+                --brentjong-space--color--highlighted: #ec0dad42;
+                --brentjong-space--color--action: #ff4538;
+                --brentjong-space--layout--padding: 2em;
+                --brentjong-space--layout--breakpoint--s: 700px;
+                --brentjong-space--border--style--thickness: 5px;
+                --brentjong-space--border--style--style: dashed;
+                --brentjong-space--border--style--color: black;
+
+                font-family: 'Droid Sans', Helvetica, Arial, sans-serif;
+                color: #000;
+                font-size: 1em;
+                line-height: 100%;
+                line-break: normal;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                min-height: calc(100vh - var(--brentjong-space--layout--padding));
+                border: calc(var(--brentjong-space--layout--padding)/2) var(--brentjong-space--color--base) solid;
+                overflow-x: hidden;
+                overflow-y: scroll;
+        }
+        @media (max-width: 700px) {
+                body {
+                        font-size: 0.75em;
+                }
+        }
+        .content {
+                padding: 11vw 2em 4em 2em;
+        }
+        @media (min-width: 1441px) {
+                .content {
+                        padding-block-start: 5vw;
+                }
+        }
+
+        .sixpack-header {position: relative;}
+
+        @media (min-width: 1020px) {
+                .sixpack-header hgroup {
+                        position: absolute;
+                        top: -3vw;
+                        right: -3vw;
+                        z-index: 3;
+
+                        background-color: var(--brentjong-space--color--base);
+                        padding: 1em;
+                        border: var(--brentjong-space--border--style--thickness) var(--brentjong-space--border--style--style) var(--brentjong-space--border--style--color);
+
+                        h1 { 
+                                padding-block-end: 0; 
+                                /* .bottle { display: none; } */
+                        }
+                }
+        }
+        @media (min-width: 1681px) {
+                .sixpack-header hgroup {
+                        top: -2.5vw;
+                        right: -4.5vw;
+                }
+        }
+        img {
+                max-width: 100%;
+                aspect-ratio: auto 1000 / 667;
+                z-index: 1;
+        }
+        img.sixpack {
+                transition: filter 1.1s ease-out;
+                /* no transitions if user prefers-reduced-motion */
+                @media (prefers-reduced-motion) {
+                        transition-property: none;
+                }
+                &:is(:hover, :focus) {
+                        cursor: pointer;
+                        transition: filter 0.35s ease-in;
+                        /* no transitions if user prefers-reduced-motion */
+                        @media (prefers-reduced-motion) {
+                                transition-property: none;
+                        }
+                }
+        }
+        img.sixpack:not(.original) {
+                filter: hue-rotate(81deg) invert(100%) brightness(31%) contrast(125%);
+                
+                &:is(:hover, :focus) {
+                        filter: hue-rotate(-273deg) invert(100%) brightness(61%);
+                }
+        }
+
+        picture { 
+                margin-inline-start: -3em;
+                margin-inline-end: -3em;
+                margin-block-end: 3em;
+                display: inline-flex;
+        }
+
+        @media (min-width: 1020px) {
+                picture {
+                        border: var(--brentjong-space--border--style--thickness) var(--brentjong-space--border--style--style) var(--brentjong-space--border--style--color);
+                        margin-inline-start: -5vw;
+                        margin-inline-end: inherit;
+                }  
+        }
+        @media (min-width: 1681px) {
+                picture {
+                        margin-inline-start: -7vw;   
+                }  
+        }
+
+        p, ul, h2, h3  {
+                font-size: 2em;
+                line-height: 130%;
+                max-width: 50ch;
+                word-wrap: break-word;
+                padding-block-end: calc(0.5 * var(--brentjong-space--layout--padding));
+        }
+        input[type="text"], input[type="email"]  {
+                font-size: 2em;
+                word-wrap: break-word;
+                margin-block-end: calc(0.5 * var(--brentjong-space--layout--padding));
+
+                border: 3px solid black;
+                padding: 3px 10px;
+        }
+        li { padding-block-start: .5em; }
+        ul {
+            padding-inline-start: 1em;
+        }
+        ul ul {
+            font-size: 1em;
+        }
+        h1, h2, h3, h4 {
+                padding-block-end: 0.5em;
+                font-weight: bold;
+                line-height: 120%;
+                hyphens: auto;
+                -webkit-hyphens: auto;
+        }
+        h1 {
+                font-size: 3em;
+                word-break: break-word;
+        }
+        h2 { font-size: 1.33em; }
+        @media (min-width: 500px) {
+                h1 .force-break {
+                        display: block;
+                }
+        }
+        .content:last-child {
+                padding-block-end: 0;
+        }
+        p.universe {
+                padding-block-end: var(--brentjong-space--layout--padding);
+        }
+        .love-list { margin-inline-start: 1.2em; }
+        @media (max-width: 700px) {
+                .love-list li {
+                        padding-block-end: 0.5em;
+                }
+                .love-list .dates {
+                        display: block;
+                }
+        }
+        .love-list li::marker {
+                content: '🚀 ';
+        }
+        
+        .love-list--past li + li {
+                padding-block-start: 0.75em;
+        }
+        .love-list--past li::marker {
+                content: '❤️ ';
+        }
+        .love-list--past .dates {
+                display: block;
+        }
+        .love-list--past .space {
+                display: block;
+        }
+        .love-list--past .space .tasks::before {
+                content: '|';
+                margin-inline-end: .4em;
+        }
+        .love-list li:hover::marker, .love-list li:focus::marker {
+                animation: rocket-launch 0.25s infinite 0.8s;
+                position: absolute;
+        }
+        .love-list--package { margin-inline-start: .3em; }
+        .love-list--package li::marker {
+                content: '❤️‍🔥 ';
+        }
+        .love-list--package > li:first-child::marker {
+                content: '❤️ ';
+        }
+        .dates--not-fix {
+                text-decoration: wavy underline;
+        }
+        
+        /* Link style v1 */
+        /* a, a:visited, a:hover {
+                text-decoration: underline;
+                text-decoration-thickness: 0.2rem;
+                color: inherit;
+                text-shadow: 0 0 5px var(--brentjong-space--color--highlighted);
+        }
+        a:hover, a:focus {
+                color: var(--brentjong-space--color--action);
+        } */
+        /* Link style v2 */
+        a, a:focus, a:hover, a:visited {
+                color: inherit;
+                text-decoration: none;
+                text-shadow: 0 0 5px var(--brentjong-space--color--highlighted);
+        }
+        a {
+                display: inline;
+                background-image: linear-gradient(#000,#000);
+                background-repeat: no-repeat;
+                background-position: 100% 100%;
+                background-size: 100% 0.1em;
+                transition: background 0.8s cubic-bezier(.77,0,.175,1), color 0.8s cubic-bezier(.77,0,.175,1);
+        }
+
+        a:focus, a:hover {
+                color: var(--brentjong-space--color--action);
+                background-size: 0 0.1em;
+                transition: background 1s cubic-bezier(.645,.045,.355,1), color 1s cubic-bezier(.645,.045,.355,1);
+        }
+
+        .link .link__satellite {
+                font-style: normal;
+                position: absolute;
+                transform: translate(-9999px, 0);
+        }
+        .link:hover .link__satellite {
+                animation: spinning-edgy 1.9s infinite;
+                /* animation: spinning-harmonious 1.3s infinite; */
+        }
+        .link .link__base-station {
+                position: relative;
+                display: inline-block;
+        }
+        .link:hover .link__base-station {
+                animation: flip-horizontal 1.9s infinite;
+        }
+        .link:hover .link__rocket-launch {
+                display: inline-block;
+                animation: rocket-launch 0.25s infinite 0.8s;
+        }
+        .credits {
+                padding-block-start: var(--brentjong-space--layout--padding);
+        }
+        .credits * {
+                font-size: 0.5em;
+        }
+
+        @media (prefers-reduced-motion) {
+                .link:hover .link__satellite {
+                        animation: spinning-no 1s;
+                }
+        }
+        @keyframes spinning-edgy {
+                0% {
+                        transform: translate(-1em, -1.9em) rotate(13deg);
+                }
+
+                23% {
+                        transform: translate(3em, 0);
+                }
+                31% {
+                        transform: translate(2.6em, 4em) rotate(7deg);
+                }
+
+                45% {
+                        transform: translate(-2.6em, 2.5em);
+                }
+
+                66% {
+                        transform: translate(-4em, 0.5em) rotate(7deg);
+                }
+
+                75% {
+                        transform: translate(-3.5em, -3em) rotate(8deg);
+                }
+
+                100% {
+                        transform: translate(-1em, -1.9em) rotate(3deg);
+                }
+        }
+        @keyframes spinning-harmonious {
+                0% {
+                        transform: translate(-1em, -1.9em) rotate(30deg);
+                }
+
+                100% {
+                        transform: translate(-1em, -1.9em) rotate(390deg);
+                }
+        }
+        @keyframes spinning-none {
+                0% {}
+
+                100% {}
+        }
+        @keyframes flip-horizontal {
+                0% { transform: translate(0, 0) rotate(0deg); }
+                25% { transform: translate(-5px, 0) rotate(5deg); }
+                49% { transform: scaleX(1) }
+                50% { transform: translate(-10px, -3px) rotate(0eg); }
+                51% { transform: scaleX(-1) }
+                74% { transform: scaleX(-1) }
+                75% { transform: translate(-5px, 0) rotate(-5deg); }
+                100% { transform: translate(0, 0) rotate(0deg); }
+        }
+        @keyframes rocket-launch {
+                0% { transform: translate(0, 0) rotate(0deg); }
+                25% { transform: translate(-5px, 0) rotate(5deg); }
+                50% { transform: translate(-10px, -3px) rotate(0eg); }
+                75% { transform: translate(-5px, 0) rotate(-5deg); }
+                100% { transform: translate(0, 0) rotate(0deg); }
+        }
+        .hidden {
+                display: none;
+        }
+        </style>
+</head>
+<body>
+        <main class="content">
+                <div class="sixpack-header">
+                        <hgroup>
+                                <h2>Limited outer space edition of 250</h2>
+                                <h1>Sixpack of Love <span class="bottle">👾</span></h1>
+                        </hgroup>
+                        <picture>
+                                <source srcset="/imgs/sixpack.avif" type="image/avif" media="(min-width: 2561px)" width="2000px" />
+                                <source srcset="/imgs/sixpack.avif" type="image/avif" media="(min-width: 1681px)" width="1520px" />
+                                <source srcset="/imgs/sixpack.jpg" media="(min-width: 1681px)" width="1520px" />
+                                <source srcset="/imgs/sixpack-small.avif" type="image/avif" />
+                                <img class="sixpack" tabindex="0" src="/imgs/sixpack-small.jpg" alt="Sixpack of Love Karton mit 6 Weinflaschen" width="1000" />
+                        </picture>
+
+                        <!-- https://youtu.be/9OdxGR5EyzA -->
+
+                </div>
+                <p>Im Herbst 2022 erntet Satellites of Love Association Pinot Noir Trauben 🍇 in Brentjong und bringt sie nach La Neuveville zu Fabien Henriot, in Leuk zu Vin d'oeuvre, nach Bern zu Bruno Bucher, in Ayent zu Chèrouche, nach Zürich zur Domaine Baby Angel und ins Bahnhöfli Ostermundigen zu Satellites of Love Association.</p>
+                <p>Et voilà: Vanessa Atalanta, Funkloch, Ciervo, Balsam, Pinot D'angio, Oh le lux!<br>
+                Sechs wild vergorene Pinot Noir Interpretationen — mal ungestüm, mal elegant, mal Bubbles, mal rot, mal rosé. It is what it is!</p>
+                <p>Willst du dir eines der raren Sixpacks sichern, dann reserviere und bestelle dir jetzt deine Sixpack Variante 🍾</p>
+                <ul class="love-list love-list--package">
+                    <li>Bestelle dir dein <a href="https://www.trallala-weine.ch/shop/typ/weinkiste/sixpack-of-love/" title="Bestelle dir dein Sixpack of Love im Trallala - CHF 168 pro Sixpack">Sixpack of Love</a> ❤️ im Trallala</li>
+                    <li>Ich möchte lieber ein <a href="https://www.trallala-weine.ch/shop/typ/weinkiste/sixpack-of-love-supreme/" title="Bestelle dir dein Sixpack of Love Supreme im Trallala und unterstütze das Projekt als Gönner*in - CHF 250 pro Sixpack">Sixpack of Love Supreme</a> ❤️‍🔥 und unterstütze das Projekt zusätzlich als Gönner*in</span>
+                        <ul>
+                            <li>dafür pflanzen 🧑‍🌾 Satellites of Love einen Strauch 🌱 / Wildobst 🌳 zur Er­hö­hung der Biodiversität Brentjongs</li>
+                            <li>du erhälst ein Poster 📓✧˖°. von Satellites of Love</li>
+                            <li>und wirst auf ein Apéro 🍷 eingeladen 🥂</li>
+                            <li>plus kriegst 1.5 Karma-Punkte 👾</li>
+                        </ul>
+                    </li>
+                </ul>
+                <p>
+                        <a title="Brentjong dot Space" href="/">Brentjong.space</a> — a growing permaculture space curated by
+                </p>
+                <p lang="en" class="universe">
+                        <span role="presentation">🛸　　　 　🌎</span><a class="link" title="Satellites of Love. Visit us on Instagram." href="https://www.instagram.com/satellites.of.love" rel="nofollow"><span class="link__base-station" role="img" aria-label="Satellite base station">📡</span> of <span role="img" aria-label="Love">❤️</span><em class="link__satellite">🛰<em class="link__satellite">❤️<em class="link__satellite">🛰<em class="link__satellite">❤️</em></em></em></a><span role="presentation">　°　　🌓　•　　.°•　　　🚀 ✯
+                        <br>
+                        　　　★　*　　　　　°　　　　🛰 　°·　　                           🪐
+                        <br>
+                        .　　　•　° ★　•  ☄</span>
+                </p>
+
+                <footer lang="en">
+                        <p class="credits"><span>Made with ❤️‍🔥 &nbsp;by&nbsp;</span><a href="https://yokai.ch">Yokai</a></p>
+                </footer>
+        </main>
+        <script type="text/javascript">
+                const imgs = document.querySelectorAll('img');
+
+                imgs.forEach(img => {
+                        img.onclick = () => img.classList.toggle('original');
+                        img.ondblclick = () => img.classList.toggle('original');
+
+                        img.addEventListener('keypress', function (e) {
+                                if (e.key === 'Enter') {
+                                        img.classList.toggle('original');
+                                }
+                        });
+                });
+        </script>
+</body>
+</html>
