72b73e7ef132b8709291e316dbaac330151bb098
Michael Zumstein <zumi@yokai.ch>
2024-11-08 17:38:21 +0100

    website update

---
+++ lean/2023/sixpack-of-love.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+        <!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans&display=swap"/> -->
+        <link rel="canonical" href="http://brentjong.space/2023/subscription"/>
+        <title>Satellites of Love Sixpack 👾 2023 — Limited outer space edition</title>
+        <meta name="view-transition" content="same-origin">
+        <meta name="description" content="Sixpack of Love 👾 2023 — Limited outer space edition of 250 sixpacks">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width">
+        <meta name="geo.region" content="CH-BE" />
+        <meta name="geo.placename" content="Bern" />
+        <meta name="geo.position" content="46.950256;7.443965" />
+        <meta name="ICBM" content="46.950256, 7.443965" />
+        <meta name="geo.region" content="CH-VS" />
+        <meta name="geo.placename" content="Brentjong, Leuk/Loèche, Wallis/Valais" />
+        <meta name="geo.position" content="46.317660;7.652257" />
+        <meta name="ICBM" content="46.317660, 7.652257" />
+        <meta property="og:site_name" content="brentjong.space">
+        <meta property="og:title" content="Sixpack of Love 👾 2023 — Limited outer space edition" />
+        <meta property="og:description" content="Order your limited outer space edition sixpack and support Satellites of Love." />
+        <meta property="og:image" itemprop="image" content="https://brentjong.space/apple-touch-icon.png">
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="de_CH" />
+        <meta name="theme-color" content="#3c0064"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="stylesheet" href="/styles/styles.css" />
+        <link rel="stylesheet" href="../node_modules/lite-youtube-embed/src/lite-yt-embed.css" />
+        <script src="../node_modules/lite-youtube-embed/src/lite-yt-embed.js"></script>
+</head>
+<body>
+        <main class="content">
+                <div class="sixpack-header">
+                        <hgroup>
+                                <h2>Limited outer space edition</h2>
+                                <h1>Sixpack of Love <span class="emoji">👾</span></h1>
+                        </hgroup>
+                        <picture>
+                                <source srcset="/imgs/sixpack-2023.jpg" type="image/jpg" media="(min-width: 2561px)" width="2000px" />
+                                <source srcset="/imgs/sixpack-2023.jpg" type="image/jpg" media="(min-width: 1681px)" width="1520px" />
+                                <source srcset="/imgs/sixpack-2023.jpg" media="(min-width: 1681px)" width="1520px" />
+                                <source srcset="/imgs/sixpack-small-2023.jpg" type="image/jpg" />
+                                <img class="sixpack" tabindex="0" src="/imgs/sixpack-small-2023.jpg" alt="Sixpack of Love Karton mit 6 Weinflaschen" width="1000" />
+                        </picture>
+                </div>
+                <ul class="love-list love-list--package">
+                    <li>Bestelle dir dein <a href="https://www.trallala-weine.ch/shop/typ/weinkiste/sixpack-of-love/" title="Bestelle dir dein Sixpack of Love im Trallala - CHF 168 pro Sixpack">Sixpack of Love</a> ❤️ im Trallala</li>
+                    <li>Ich möchte lieber ein <a href="https://www.trallala-weine.ch/shop/typ/weinkiste/sixpack-of-love-supreme/" title="Bestelle dir dein Sixpack of Love Supreme im Trallala und unterstütze das Projekt als Gönner*in - CHF 250 pro Sixpack">Sixpack of Love Supreme</a> ❤️‍🔥 und unterstütze das Projekt zusätzlich als Gönner*in</span>
+                        <ul>
+                            <li>dafür pflanzen 🧑‍🌾 Satellites of Love einen Strauch 🌱 / Wildobst 🌳 zur Er­hö­hung der Biodiversität Brentjongs</li>
+                            <li>du erhälst ein Poster 📓✧˖°. von Satellites of Love</li>
+                            <li>und wirst auf ein Apéro 🍷 eingeladen 🥂</li>
+                            <li>plus kriegst 1.5 Karma-Punkte 👾</li>
+                        </ul>
+                    </li>
+                </ul>
+                <p>
+                        <a title="Brentjong dot Space" href="/">Brentjong.space</a> — a growing permaculture space curated by
+                </p>
+                <p lang="en" class="universe">
+                        <span role="presentation">🛸　　　 　🌎</span><a class="link" title="Satellites of Love. Visit us on Instagram." href="https://www.instagram.com/satellites.of.love" rel="nofollow"><span class="link__base-station" role="img" aria-label="Satellite base station">📡</span> of <span role="img" aria-label="Love">❤️</span><em class="link__satellite">🛰<em class="link__satellite">❤️<em class="link__satellite">🛰<em class="link__satellite">❤️</em></em></em></a><span role="presentation">　°　　🌓　•　　.°•　　　🚀 ✯
+                        <br>
+                        　　　★　*　　　　　°　　　　🛰 　°·　　                           🪐
+                        <br>
+                        .　　　•　° ★　•  ☄</span>
+                </p>
+
+                <footer lang="en">
+                        <div class="credits"><span>Made with ❤️‍🔥 &nbsp;by&nbsp;</span><a href="https://yokai.ch">Yokai</a></div>
+                        <div class="legal"><a title="brentjong.space legal policy. We are not evil 😘" href="/legal">Legal</a></div>
+                </footer>
+                <div class="galaxy" role="presentation"></div>
+        </main>
+        <script type="text/javascript">
+                const imgs = document.querySelectorAll('img');
+
+                imgs.forEach(img => {
+                        img.onclick = () => img.classList.toggle('focused');
+                        img.ondblclick = () => img.classList.toggle('focused');
+
+                        img.addEventListener('keypress', function (e) {
+                                if (e.key === 'Enter') {
+                                        img.classList.toggle('focused');
+                                }
+                        });
+                });
+        </script>
+</body>
+</html>
